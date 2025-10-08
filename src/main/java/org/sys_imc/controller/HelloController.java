3fbd4261b41436593ca878b6d5633908a91307eb
kauanluiz03 <kauanluiz.souto03@gmail.com>
2025-10-08 02:05:00 -0300

    configurando botões

--- src/main/java/org/sys_imc/controller/HelloController.java
+++ src/main/java/org/sys_imc/controller/HelloController.java
@@ -3,6 +3,9 @@ package org.sys_imc.controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import org.sys_imc.model.Pessoa;
+
+import java.text.DecimalFormat;
 
 public class HelloController {
     @FXML
@@ -11,12 +14,26 @@ public class HelloController {
     public TextField txtAltura;
     @FXML
     public TextField txtPeso;
+    @FXML
+    public Label lbIMC;
+
+    Pessoa pessoa = new Pessoa();
 
 
     @FXML
     protected void onCalcularimcClick() {
-        System.out.println(this.txtNome.getText());
-        System.out.println(this.txtAltura.getText());
-        System.out.println(this.txtPeso.getText());
+
+        DecimalFormat df = new DecimalFormat();
+
+        float imc;
+        float altura;
+        float peso;
+
+        altura = Float.parseFloat(this.txtAltura.getText());
+        peso = Float.parseFloat(this.txtPeso.getText());
+        imc = peso / (altura * altura);
+
+        df.applyPattern("#0.00");
+        this.lbIMC.setText(df.format(imc));
     }
 }
